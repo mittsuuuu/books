0450187137c03900a2099f85202a3e11299639b0
mittsuuuu <mitsuru.bamitonton@gmail.com>
2025-08-04 10:58:30 +0900

    animation

---
+++ .output/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,85 @@
+const client_manifest = {
+  "_JPxgWb4k.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "JPxgWb4k.js",
+    "name": "v3",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BMzbpSUv.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_JPxgWb4k.js"
+    ],
+    "css": [
+      "error-404.DqZyKpgk.css"
+    ]
+  },
+  "error-404.DqZyKpgk.css": {
+    "file": "error-404.DqZyKpgk.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CzBOg2ZO.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_JPxgWb4k.js"
+    ],
+    "css": [
+      "error-500.CZqNkBuR.css"
+    ]
+  },
+  "error-500.CZqNkBuR.css": {
+    "file": "error-500.CZqNkBuR.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CpWt2fc_.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.CnwO3BXm.css"
+    ]
+  },
+  "entry.CnwO3BXm.css": {
+    "file": "entry.CnwO3BXm.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
+  ssr: false, // 静的サイト生成を有効化
+  app: {
+    baseURL: '/', // GitHub Pagesのルートパス
+    buildAssetsDir: '/_nuxt/' // アセットのパス
+  },
   devtools: { enabled: true },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 })
