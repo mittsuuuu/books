64456764b3be1ec3a91b2688051dfb06d676b29d
mittsuuuu <mitsuru.bamitonton@gmail.com>
2025-08-04 13:36:15 +0900

    nuxt.config.tsの変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
-export default defineNuxtConfig({
-  ssr: false, // 静的サイト生成を有効化
-  app: {
-    baseURL: './', // GitHub Pagesのルートパス
-    buildAssetsDir: '/_nuxt/' // アセットのパス
+// export default defineNuxtConfig({
+//   ssr: false, // 静的サイト生成を有効化
+//   app: {
+//     baseURL: './', // GitHub Pagesのルートパス
+//     buildAssetsDir: '/_nuxt/' // アセットのパス
+//   },
+//   devtools: { enabled: true },
+//   css: ["bootstrap/dist/css/bootstrap.min.css"],
+// })
+
+export default {
+    router: {
+    base: "/mittsuuuu.git.io/",
   },
   generate: {
     dir: 'docs'
   },
-  devtools: { enabled: true },
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
-})
+}
